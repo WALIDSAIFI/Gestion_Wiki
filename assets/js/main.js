a7d040f6f7d1b842c1ba43b515e3ef2df16fe148
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 15:29:34 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,2 @@
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
 
-    if (username.trim() === '' || email.trim() === '' || password.trim() === '') {
-        alert('Tous les champs doivent être remplis');
-        return false;
-    }
-    var emailRegex = /@gmail.com/;
-    if (!emailRegex.test(email)) {
-        alert('Veuillez entrer une adresse email valide');
-        return false;
-    }
-    return true;
-}
+
