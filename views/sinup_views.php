3634214481640f0b57ab10fe5da9efc584a66069
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 10:11:06 +0100

    Update sinup_views.php

--- views/sinup_views.php
+++ views/sinup_views.php
@@ -6,24 +6,24 @@
                     <h2 class="mb-0">Register</h2>
                 </div>
                 <div class="card-body">
-                    <form>
+                    <form action="index.php?page=login">
                         <div class="form-group">
                             <label for="username">Username:</label>
-                            <input type="text" class="form-control" id="username" placeholder="Enter your username">
+                            <input type="text" name="username" class="form-control" id="username" placeholder="Enter your username">
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email">
                         </div>
                         <div class="form-group">
                             <label for="password">Password:</label>
-                            <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password">
                         </div>
                         <button type="submit" class="btn btn-primary btn-block">Register</button>
                     </form>
                 </div>
                 <div class="card-footer text-center">
-                    <p class="mb-0">Already have an account? <a href="#" class="text-primary">Login</a></p>
+                    <p class="mb-0">Already have an account? <a href="index.php?page=login" class="text-primary">Login</a></p>
                 </div>
             </div>
         </div>
