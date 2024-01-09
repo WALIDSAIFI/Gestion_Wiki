3f271431e70b28da77d2d421e0c95e55ee59f763
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 23:16:05 +0100

    Update sinup_views.php

--- views/sinup_views.php
+++ views/sinup_views.php
@@ -6,24 +6,33 @@
                     <h2 class="mb-0">Register</h2>
                 </div>
                 <div class="card-body">
-                    <form  method="post"  action="index.php?page=sinup">
+                    <form>
                         <div class="form-group">
                             <label for="prenom">Prénom:</label>
                             <input type="text" name="prenom" class="form-control" id="prenom" placeholder="Enter votre Prénom"  onsubmit="return validateForm()">
                         </div>
+                        <span id="First_err" class="text-danger small"></span>
+
                         <div class="form-group">
                             <label for="nom">Nom:</label>
                             <input type="text" name="nom" class="form-control" id="nom" placeholder="Enter votre Nom">
                         </div>
+
+                        <span id="last_err" class="text-danger small"></span>
+
                         <div class="form-group">
                             <label for="email">Email:</label>
                             <input type="email" name="email" class="form-control" id="email" placeholder="Enter votre email">
                         </div>
+                        <span id="email_err" class="text-danger small"></span>
+
                         <div class="form-group">
                             <label for="password">Password:</label>
                             <input type="password" name="password" class="form-control" id="password" placeholder="Enter votre password">
                         </div>
-                        <button type="submit" name="registre" class="btn btn-primary btn-block">Register</button>
+                        <span id="password_err" class="text-danger small"></span>
+
+                        <button type="button" id="signup" name="signup" class="btn btn-primary btn-block">Register</button>
                     </form>
                 </div>
                 <div class="card-footer text-center">
