9be82f9766f1c867a1847174c91c17e22adb1b07
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 15:28:55 +0100

    Update sinup_views.php

--- views/sinup_views.php
+++ views/sinup_views.php
@@ -1,6 +1,6 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header bg-primary text-white text-center">
                     <h2 class="mb-0">Register</h2>
@@ -8,16 +8,20 @@
                 <div class="card-body">
                     <form  method="post"  action="index.php?page=sinup">
                         <div class="form-group">
-                            <label for="username">Username:</label>
-                            <input type="text" name="username" class="form-control" id="username" placeholder="Enter your username"  onsubmit="return validateForm()">
+                            <label for="prenom">Prénom:</label>
+                            <input type="text" name="prenom" class="form-control" id="prenom" placeholder="Enter votre Prénom"  onsubmit="return validateForm()">
+                        </div>
+                        <div class="form-group">
+                            <label for="nom">Nom:</label>
+                            <input type="text" name="nom" class="form-control" id="nom" placeholder="Enter votre Nom">
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Enter votre email">
                         </div>
                         <div class="form-group">
                             <label for="password">Password:</label>
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Enter votre password">
                         </div>
                         <button type="submit" name="registre" class="btn btn-primary btn-block">Register</button>
                     </form>
