c59b23a367e2fa2ad4ddf449f64d16b84996001e
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 15:29:10 +0100

    Update sinup_model.php

--- models/sinup_model.php
+++ models/sinup_model.php
@@ -1,9 +1,14 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['registre'])) {
-        $username = $_POST['username'];
+
+    if (isset($_POST['registre']) && isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['email'])) {
+
+        $prenom = $_POST['prenom'];
+        $nom = $_POST['nom'];
         $email = $_POST['email'];
         $password = $_POST['password'];
-        user::register($username,$email,$password);
+        user::register($prenom,$nom,$email,$password);
+
     }
 }
