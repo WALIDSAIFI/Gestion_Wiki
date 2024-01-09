fb3afd286b47295cdbb9c2d3454ee66cb7197e3d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-09 09:42:41 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./assets/image/Wiki.png" type="image/png">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.8.12/tailwind-experimental.min.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
 
 
@@ -21,6 +23,7 @@
     <footer></footer>
     <script src="<?= PATH ?>assets/js/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha384-3+GrpLQ6sa02H+sgKwFfWV/Zz+FJtvbSMfO9fs/iTC6vqUOmeaLYKaFwEEXmO7Lb" crossorigin="anonymous"></script>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-yNeBV5I5IjOBObIFW8awM6gTPQ3JC3Y3C1g9M+K3JBWc7pc/Bp1UcF7BDmNfNdf1" crossorigin="anonymous"></script>
 </body>
 </html>
