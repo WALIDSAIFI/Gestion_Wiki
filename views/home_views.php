8a5ab0e2f10c7535c777c12fcb5284359cd80283
Walid Saifi <saifiwalid21@gmail.com>
2024-01-10 14:48:37 +0100

    Update home_views.php

--- views/home_views.php
+++ views/home_views.php
@@ -45,16 +45,85 @@
                 <p>
                     <?php
                     if(isset($_SESSION['email'])) {
-                        echo '<a href="#" class="btn btn-primary my-2">Ajouter Wiki</a>';
+                        echo ' <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Ajouter un Wiki</button>';
 
                         echo '<a href="#" class="btn btn-secondary my-2">Modifier mes Wiki</a>';
                     }
                     ?>
                 </p>
 
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Ajouter un Wiki</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="recipient-name" class="col-form-label">Titre:</label>
+                                        <input type="text" class="form-control" id="titre" name="titre">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="message-text" class="col-form-label">Le contenu de Wiki:</label>
+                                        <textarea class="form-control" id="content" name="content"></textarea>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="selectMultiple" class="col-form-label">Ajouter les tag:</label>
+                                        <select multiple class="form-select" id="selectMultiple" name="selectMultiple">
+                                            <option value="option1">Option 1</option>
+                                            <option value="option2">Option 2</option>
+                                            <option value="option3">Option 3</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="selectMultiple" class="col-form-label">Ajouter la catégorie de Wiki : </label>
+                                        <select  class="form-select" id="selectMultiple" name="selectMultiple">
+                                            <option value="option1">Option 1</option>
+                                            <option value="option2">Option 2</option>
+                                            <option value="option3">Option 3</option>
+                                            =
+                                        </select>
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" id="ajouter" name="ajouter" class="btn btn-primary">Ajouter</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
     </section>
+
+
+
+
+
 
     <div class="album py-5 bg-light">
         <div class="container">
