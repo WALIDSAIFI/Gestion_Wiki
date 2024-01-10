cb1954943a3aadf3c49e42c7472320a7c223ad31
Walid Saifi <saifiwalid21@gmail.com>
2024-01-10 11:08:11 +0100

    Update login_model.php

--- models/login_model.php
+++ models/login_model.php
@@ -12,6 +12,18 @@ if (isset($_POST["login"]) && $_POST["login"] == "login") {
         echo json_encode(["errors" => $errors]);
         exit;
     }
+     $user = user::login($password,$email);
+
+      $_SESSION['role'] =$user['role'];
+      $_SESSION['id'] = $user['id'];
+      $_SESSION['email'] = $user['email'];
+
+      
+      if($user['role'] == 'admin'){
+      header("http://localhost/WALID_SAIFI_Wiki/index.php?page=dashdord");
+      }else{
+          header("http://localhost/WALID_SAIFI_Wiki/index.php?page=home");
+      }
 
     echo json_encode(["success" => "User registered successfully"]);
     exit;
