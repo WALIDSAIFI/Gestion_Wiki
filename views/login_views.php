64ccf98ed02f752e1c056c8a65368ec283e60c7d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-10 10:25:04 +0100

    Update login_views.php

--- views/login_views.php
+++ views/login_views.php
@@ -11,10 +11,13 @@
                             <label for="email">Email:</label>
                             <input type="email" class="form-control" id="email" placeholder="Enter your email">
                         </div>
+                        <span id="email_err" class="text-danger small"></span>
                         <div class="form-group">
                             <label for="password">Password:</label>
                             <input type="password" class="form-control" id="password" placeholder="Enter your password">
                         </div>
+
+                        <span id="password_err" class="text-danger small"></span>
                         <button type="button" id="login" name="login" class="btn btn-primary btn-block">Login</button>
                     </form>
                 </div>
