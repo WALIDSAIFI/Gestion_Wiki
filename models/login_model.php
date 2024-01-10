c0cd77a91f42d2bedfd0cef64fb9c26aa35870ee
Walid Saifi <saifiwalid21@gmail.com>
2024-01-10 10:09:42 +0100

    Update login_model.php

--- models/login_model.php
+++ models/login_model.php
@@ -1,1 +1,14 @@
 <?php
+global $db;
+if (isset($_POST["login"]) && $_POST["login"] == "login") {
+    $prenom = $_POST["prenom"];
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $errors = [
+        "email_err" => Validation::validateEmail($email),
+        "password_err" => Validation::validatePassword($password),
+    ];
+
+}
+
