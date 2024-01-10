3b9863df1505ac3887a98d4ddd8402aa77f46d02
Walid Saifi <saifiwalid21@gmail.com>
2024-01-10 14:48:34 +0100

    Create wiki.php

---
+++ _classes/wiki.php
@@ -0,0 +1,60 @@
+<?php
+
+class Article
+{
+
+    public $title;
+    public $content;
+    public $createdAt;
+    public $editAt;
+    public $status;
+    public $idUser;
+    public $idCategory;
+
+
+    public function __construct($id)
+    {
+        global $db;
+
+        $sql = "SELECT * FROM articles WHERE id = :id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $article = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+        $this->title = $article['title'];
+        $this->content = $article['content'];
+        $this->createdAt = $article['create_at'];
+        $this->editAt = $article['edit_at'];
+        $this->status = $article['status'];
+        $this->idUser = $article['id_user'];
+        $this->idCategory = $article['id_category'];
+    }
+
+    public function ajouterArticle($title, $content, $idUser, $idCategory)
+    {
+            global $db;
+
+            $sql = "INSERT INTO articles (title, content, create_at, id_user, id_category, status) 
+                    VALUES (:title, :content, NOW(), :idUser, :idCategory, 'published')";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+            $stmt->bindParam(':idUser', $idUser, PDO::PARAM_INT);
+            $stmt->bindParam(':idCategory', $idCategory, PDO::PARAM_INT);
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
