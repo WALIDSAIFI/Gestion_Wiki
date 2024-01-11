62693d42c22501a5b14a4a162e6bc331ed1ae448
Walid Saifi <saifiwalid21@gmail.com>
2024-01-11 15:16:01 +0100

    Update tags.js

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,25 +1,42 @@
+console.log("hello youcode");
 
-const titre = document.getElementById('titre').value;
+const ajouter = document.getElementById('ajouter');
 
-var formData = new FormData();
-formData.append('titre', titre);
+ajouter.addEventListener('click', () => {
 
-$.ajax({
-    type: "POST",
-    url: "http://localhost/WALID_SAIFI_Wiki/index.php?page=dashbord",
-    data: formData,
-    processData: false,
-    contentType: false,
-    success: (response) => {
+    const titre = document.getElementById('titre').value;
+    console.log(titre);
+    var formData = new FormData();
+    formData.append('titre', titre);
+    formData.append('ajouter',ajouter);
+    console.log(formData);
 
-        const data = JSON.parse(response);
-        
-    },
-    error: (error) => {
-        console.log(error);
-    }
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/WALID_SAIFI_Wiki/index.php?page=dashbord",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (response) => {
+            const data = JSON.parse(response);
+            console.log(data);
+
+            if (data.success) {
+                alert(data.success);
+                document.getElementById('titre').value = "";
+            }else if (data.errors) {
+                var err= data.errors;
+                if(err.titre!= false){
+                    $("#titre_err").text(err.titre);
+                }
+
+            }
+
+
+
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    });
 });
-
-
-
-
