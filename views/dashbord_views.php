a4ee407f48cc064a4bc8b237108cf90ba4c4b42a
Walid Saifi <saifiwalid21@gmail.com>
2024-01-11 21:49:10 +0100

    Update dashbord_views.php

--- views/dashbord_views.php
+++ views/dashbord_views.php
@@ -3,27 +3,28 @@
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
+            <a class="btn btn-danger px-3" href="#">Déconnexion</a>
         </div>
+
     </div>
 </header>
 
 <div class="container-fluid">
-    <div class="row ">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="row  w-35 p-5">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse pl-3">
             <div class="position-sticky pt-3">
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2">
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tag" data-bs-whatever="@mdo">Ajouter un Tag</button>
+                        <button type="button" class="btn btn-primary btn-block" data-bs-toggle="modal" data-bs-target="#tag" data-bs-whatever="@mdo">Ajouter un Tag</button>
                     </li>
                     <li class="nav-item mb-2">
                         <!-- Ajoutez ici votre deuxième élément de navigation ou laissez vide si nécessaire -->
                     </li>
                     <li class="nav-item mb-2">
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Ajouter une Catégorie</button>
+                        <button type="button" class="btn btn-primary btn-block" data-bs-toggle="modal" data-bs-target="#categories" data-bs-whatever="@mdo">Ajouter une Catégorie</button>
                     </li>
                     <li class="nav-item mb-2">
                         <a class="nav-link" href="#">
@@ -40,6 +41,7 @@
                 </ul>
             </div>
         </nav>
+
 
 
 
@@ -69,6 +71,34 @@
             </div>
         </div>
 
+
+
+
+        <div class="modal fade" id="categories" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ajouter un categories</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">nom de categories:</label>
+                                <input type="text" class="form-control" id="nom_categories" name="nom_categories">
+                            </div>
+                            <span id="categories_err" class="text-danger small"></span>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" id="ajouter_categories" name="ajouter_categories" class="btn btn-primary">Ajouter</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
 
 
