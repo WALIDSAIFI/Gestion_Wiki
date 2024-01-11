5f02f5b6be3b5f33b25aeb6879244433f40739ba
Walid Saifi <saifiwalid21@gmail.com>
2024-01-11 15:23:42 +0100

    ajouter class categories

---
+++ _classes/Categorie.php
@@ -0,0 +1,27 @@
+<?php
+
+class Categorie
+{
+    private $id;
+    private $nom;
+
+    public function __construct($id) {
+        global $db;
+        $sql = "SELECT * FROM categories WHERE id = :id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $categorie = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->id = $categorie['id'];
+        $this->nom = $categorie['nom'];
+    }
+
+    static public function ajouterCategorie($nom){
+        global $db;
+        $sql = "INSERT INTO categories(nom) VALUES (:nom)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+}
