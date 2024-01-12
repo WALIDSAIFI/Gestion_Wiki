dc15f42dd6c51ec3e946358e71907eb081a8b2dd
Walid Saifi <saifiwalid21@gmail.com>
2024-01-12 10:20:39 +0100

    Update home_model.php

--- models/home_model.php
+++ models/home_model.php
@@ -5,3 +5,24 @@ if (isset($_POST['logout'])) {
     header("Location: index.php?page=home");
     exit();
 }
+
+if(isset($_POST['ajouter_wiki'])){
+
+    $titre = $_POST['titre'];
+    $content = $_POST['content'];
+    $selectedTags = $_POST['selectedTags'];
+    $category = $_POST['category'];
+
+    $errors = [
+        "titre" => Validation::validatetitre($titre),
+        "content" => Validation::validateContent($content),
+    ];
+
+
+    if (array_filter($errors)) {
+
+        echo json_encode(["errors" => $errors]);
+
+        exit;
+    }
+}
