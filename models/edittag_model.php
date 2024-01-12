f16868a6864c38e484109096dae7b8fd6c736d6d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-13 00:50:04 +0100

    Create edittag_model.php

---
+++ models/edittag_model.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_GET['id'])){
+
+    $id=$_GET['id'];
+    $tag = Tag::get_tag($id);
+
+}
+if(isset($_POST['modifier'])){
+    $id=$_GET['id'];
+   $name = $_POST['titre'];
+   Tag::update_tag($id,$name);
+
+
+    header("http://localhost/WALID_SAIFI_Wiki/index.php?page=dashboard");
+
+
+}
