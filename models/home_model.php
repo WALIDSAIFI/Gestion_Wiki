4e62892376015a4e94cab8f511802130351cedcb
Walid Saifi <saifiwalid21@gmail.com>
2024-01-13 23:52:15 +0100

    Update home_model.php

--- models/home_model.php
+++ models/home_model.php
@@ -12,6 +12,7 @@ if(isset($_POST['ajouter_wiki'])){
     $content = $_POST['content'];
     $selectedTags = $_POST['selectedTags'];
     $category = $_POST['category'];
+    $selectedTags = explode(',', $selectedTags);
 
     $errors = [
         "titre" => Validation::validatetitre($titre),
@@ -24,7 +25,19 @@ if(isset($_POST['ajouter_wiki'])){
 
         exit;
     }
- wiki::ajouterArticle($titre,$content,$id_user,$category);
+
+    $id_article=wiki::ajouterArticle($titre,$content,$id_user,$category);
+    foreach ($selectedTags as $id_Tag) {
+        global $db;
+        $sql = "INSERT INTO articles_tags (id_article, id_tag) VALUES (:id_article, :id_tag)";
+        $stm = $db->prepare($sql);
+        $stm->bindParam(':id_article', $id_article, PDO::PARAM_INT);
+        $stm->bindParam(':id_tag', $id_Tag, PDO::PARAM_INT);
+        $stm->execute();
+    }
+
+
+
 }
 
 $latestWiki = wiki::getTheLatestWiki();
