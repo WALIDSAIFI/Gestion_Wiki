e75f733961258ab9612818273cef3a9b99a0b560
Walid Saifi <saifiwalid21@gmail.com>
2024-01-13 20:43:16 +0100

    Update editcategorie_model.php

--- models/editcategorie_model.php
+++ models/editcategorie_model.php
@@ -1,20 +1,24 @@
 <?php
-if(isset($_GET['id'])){
-    $id=$_GET['id'];
-    $cat = Categorie::get_categorie($id);
-}
-
-if(isset($_POST['modifier'])){
-    $id=$_GET['id'];
-    $name = $_POST['cat'];
-    Categorie::update_catigo($id,$name);
-    header("Location: http://localhost/WALID_SAIFI_Wiki/index.php?page=dashboard");
 
 
-
-}
 
 $nbrCat=Categorie::getNombreCategories();
 $nbrTag = Tag::getNombreTags();
 $nbruser = user::getNombreUtilisateurs();
 $nbrwiki  = Wiki::getNombreWiki();
+
+
+
+$id = $_GET['id_cat'];
+$cat = Categorie::get_categorie($id);
+
+
+if (isset($_POST['modifier_cat'])) {
+    $name = $_POST['cat'];
+
+
+    Categorie::update_categorie($id, $name);
+
+    header("Location: index.php?page=dashbord");
+}
+
