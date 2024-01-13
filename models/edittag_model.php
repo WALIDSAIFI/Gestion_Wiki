ad03beeba54099cd59ee2dd785301dc07df0f33d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-13 20:43:20 +0100

    Update edittag_model.php

--- models/edittag_model.php
+++ models/edittag_model.php
@@ -1,17 +1,20 @@
 <?php
 if(isset($_GET['id'])){
-
     $id=$_GET['id'];
     $tag = Tag::get_tag($id);
-
 }
+
+
+
+
 if(isset($_POST['modifier'])){
 
     $id=$_GET['id'];
-   $name = $_POST['titre'];
+    $name = $_POST['titre'];
    Tag::update_tag($id,$name);
 
-    header("Location: http://localhost/WALID_SAIFI_Wiki/index.php?page=dashboard");
+    header("Location: index.php?page=dashbord");
+
 }
 
 $nbrCat=Categorie::getNombreCategories();
