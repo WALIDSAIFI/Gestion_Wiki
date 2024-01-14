95d2cc8c7495dd730b5a63811040a7aab54f026a
Walid Saifi <saifiwalid21@gmail.com>
2024-01-14 15:31:50 +0100

    Update home_model.php

--- models/home_model.php
+++ models/home_model.php
@@ -41,3 +41,22 @@ if(isset($_POST['ajouter_wiki'])){
 }
 
 $latestWiki = wiki::getTheLatestWiki();
+
+
+
+if(isset($_GET["search_title"])) {
+    $input_value = $_GET["input_value"];
+    $searchedData = Search::searchForTitles($input_value);
+
+    echo json_encode($searchedData);
+    exit;
+}
+
+if(isset($_GET["search_tag"])) {
+    $input_value = $_GET["input_value"];
+    $searchedData = Search::searchForTags($input_value);
+
+    echo json_encode($searchedData);
+    exit;
+}
+
