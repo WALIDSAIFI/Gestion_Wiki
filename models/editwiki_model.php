c4b75991bf64dec45c7f22bff1e44b19fd8bbc6d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-14 13:43:41 +0100

    Create editwiki_model.php

---
+++ models/editwiki_model.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $wiki = wiki::get_Wiki_pour_edit($id);
+   // dd($wiki);
+
+}
+if(isset($_POST['editWiki'])){
+    $id = $_GET['id'];
+    $titre = $_POST['title'];
+    $content = $_POST['content'];
+    $tag = $_POST['tags'];
+    $categoier = $_POST['category'];
+
+
+    if (!is_array($tag)) {
+        $tag = [$tag];
+    }
+
+    wiki::updateArticle($id, $titre, $content, $categoier, $tag);
+
+    header("Location: index.php?page=meswiki");
+}
