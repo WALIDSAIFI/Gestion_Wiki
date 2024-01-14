477eb634a3641d350e25170f6983f993283c6fa6
Walid Saifi <saifiwalid21@gmail.com>
2024-01-14 01:08:08 +0100

    Update deswiki_model.php

--- models/deswiki_model.php
+++ models/deswiki_model.php
@@ -2,4 +2,8 @@
 if(isset($_GET['id']) ){
     $id =$_GET['id'];
     $article = wiki::get_Wiki($id);
+ //   print_r($article);
+    $catgoier = wiki::getCategoryDetails($id);
+    $tags = wiki::getTagsDetails($id);
+
 }
